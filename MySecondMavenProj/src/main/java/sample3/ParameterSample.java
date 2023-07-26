64bb74fb8f5add2dd902a748ec4c54000d534a73
sajit <sajit@LAPTOP-L3IGT96C>
2023-07-25 22:09:46 -0400

    first commit

---
+++ MySecondMavenProj/src/main/java/sample3/ParameterSample.java
@@ -0,0 +1,21 @@
+package sample3;
+
+public class ParameterSample {
+	 public static void print(String name,int x)
+	 {
+	 System.out.println(name);
+	 System.out.println(x);
+	 }
+	 public static void add(int a,int b)
+	 {
+		  System.out.println(a+b);
+	 }
+	public static void main(String args[])
+	{
+	ParameterSample.print("Devu",9);
+	add(5,7);
+	}
+
+	
+
+}
